5ed41e2f563af9176580ce1b357966896b65bcda
AlenMraz <mraz.alen@gmail.com>
2025-01-03 19:24:41 +0100

    camera takes 480x480 pic

--- RG projektna/src/camera.js
+++ RG projektna/src/camera.js
@@ -1,17 +1,27 @@
 import * as THREE from "three";
 function takePictureFromCamera(cameraToCapture, renderer, scene) {
+    const originalWidth = renderer.domElement.width;
+    const originalHeight = renderer.domElement.height;
+
+    renderer.setSize(480, 480);
+    cameraToCapture.aspect = 1; 
+    cameraToCapture.updateProjectionMatrix(); 
+
     renderer.render(scene, cameraToCapture);
-  
+
     const imageData = renderer.domElement.toDataURL("image/png");
-    // debuging
-    // Create a link to download the image
+
+    // Debugging: Create a link to download the image
     // const link = document.createElement("a");
     // link.href = imageData;
     // link.download = "camera_capture.png";
     // link.click();
-  
-    // Return the image data (optional)
+
+    renderer.setSize(originalWidth, originalHeight);
+    cameraToCapture.aspect = originalWidth / originalHeight; 
+    cameraToCapture.updateProjectionMatrix();
+
     return imageData;
-  }
-  
+}
+
 export { takePictureFromCamera };
