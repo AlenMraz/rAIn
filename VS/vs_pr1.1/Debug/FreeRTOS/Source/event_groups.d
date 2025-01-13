b2f8f3ca936abab31348a93285828e16aa41ec83
neza <neza@xps13.urbas.si>
2025-01-13 11:32:52 +0100

    FreeRTOS opravila

---
+++ VS/vs_pr1.1/Debug/FreeRTOS/Source/event_groups.d
@@ -0,0 +1,27 @@
+FreeRTOS/Source/event_groups.o: ../FreeRTOS/Source/event_groups.c \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/FreeRTOS.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/FreeRTOSConfig.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/projdefs.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/portable.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/deprecated_definitions.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/mpu_wrappers.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/list.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/timers.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/event_groups.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/timers.h
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/FreeRTOS.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/FreeRTOSConfig.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/projdefs.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/portable.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/deprecated_definitions.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/mpu_wrappers.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/list.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/timers.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/event_groups.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/timers.h:
---
+++ VS/vs_pr1.1/Debug/FreeRTOS/Source/queue.d
@@ -0,0 +1,23 @@
+FreeRTOS/Source/queue.o: ../FreeRTOS/Source/queue.c \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/FreeRTOS.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/FreeRTOSConfig.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/projdefs.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/portable.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/deprecated_definitions.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/mpu_wrappers.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/list.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/queue.h \
+ D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/FreeRTOS.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/FreeRTOSConfig.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/projdefs.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/portable.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/deprecated_definitions.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/mpu_wrappers.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/list.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/queue.h:
+D:/stm32/STM32CubeIDE_1.16.1/vs_pr1.1/FreeRTOS/Source/include/task.h:
