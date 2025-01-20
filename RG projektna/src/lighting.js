905e397ee19d93aa6e92c724429a896268add7b7
AlenMraz <72938492+AlenMraz@users.noreply.github.com>
2025-01-20 16:17:29 +0100

    updated lighting

--- RG projektna/src/lighting.js
+++ RG projektna/src/lighting.js
@@ -1,8 +1,14 @@
 import * as THREE from "three";
 function initLighting(scene) {
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(5, 5, -5);
-  scene.add(directionalLight);
+  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight1.position.set(5, 5, -5);
+  scene.add(directionalLight1);
+  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight2.position.set(5, 5, 5);
+  scene.add(directionalLight2);
+  const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight3.position.set(-5, 5, 0);
+  scene.add(directionalLight3);
   scene.add(new THREE.AmbientLight(0x404040));
 }
 export { initLighting };
